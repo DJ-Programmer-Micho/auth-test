daebcfaed801d6842447ba6230e9440e94aab408
DJ-Programmer-Micho <63083701+DJ-Programmer-Micho@users.noreply.github.com>
2023-06-12 00:56:54 +0300

    terminator.bat

--- terminator.bat
+++ terminator.bat
@@ -3,8 +3,11 @@
 REM Step 1
 copy routes\dtyroute.txt routes\web.php
 
-REM Step 2: Delete the batch file
-del /F "path/to/setupMET.bat"
+REM Step 2: Delete the txt file
+del routes\dtyroute.txt
 
-REM Step 2: Delete the batch file
-del /F "path/to/terminator.bat"
+REM Step 3: Delete the batch file
+del setupMET.bat
+
+REM Step 4: Delete the batch file
+del terminator.bat
