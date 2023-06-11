f856a067b36ddbbe5d0b2b7e7040111c0b7836e8
DJ-Programmer-Micho <63083701+DJ-Programmer-Micho@users.noreply.github.com>
2023-06-11 22:12:01 +0300

    Update setupMET.bat

--- setupMET.bat
+++ setupMET.bat
@@ -25,7 +25,5 @@ REM Step 7 (Please provide the actual URL for the setup page)
 start http://127.0.0.1:8000/metiraq/setup
 
 REM Step 8
-start "" "cmd /k php artisan serve"
+start "cmd /k php artisan serve"
 
-REM Step 9
-copy routes\dtyroute.txt routes\web.php
